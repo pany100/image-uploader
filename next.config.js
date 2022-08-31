e6c749a38297848a78a7aa5589802b67a157ec69
Luis Paniagua <luispaniagua@MacBook-Pro-de-Luis.local>
2022-08-31 20:36:53 -0300

    setting up linaria

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+const withLinaria = require("next-linaria");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withLinaria(nextConfig);
